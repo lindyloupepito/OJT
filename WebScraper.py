27bd22dfddab85b949e74f1df2bde825a4a96a9e
Lindy Lou Pepito <lindyloupepito@gmail.com>
2014-05-08 23:10:48 -0700

    Update WebScraper.py

--- WebScraper.py
+++ WebScraper.py
@@ -1,24 +1,22 @@
 from bs4 import BeautifulSoup
 from collections import OrderedDict
-import requests
-import json
-import multiprocessing
-import logging
-import time
+from splinter import Browser
+import requests, json, multiprocessing, logging, time
+
 logging.basicConfig(filename="extracted_data.json", level=logging.WARNING, format='%(message)s')
 requests_log = logging.getLogger("requests")
 
-class WebScraper:
-	def __init__(self):
-		self.proxies = {
-			"http": "http://23.27.197.200:24801",
-			"http": "http://23.27.197.201:24801",
-			"http": "http://23.27.197.202:24801",
-			"http": "http://23.27.197.203:24801",
-			"http": "http://23.27.197.204:24801",
-			"http": "http://23.27.197.205:24801"
-		}
+proxies = [
+	"--proxy=23.27.197.200:24801",
+	"--proxy=23.27.197.201:24801",
+	"--proxy=23.27.197.202:24801",
+	"--proxy=23.27.197.203:24801",
+	"--proxy=23.27.197.204:24801",
+	"--proxy=23.27.197.205:24801",
+	"--proxy-type=http",
+]
 
+class WebScraper:
 	def search(self, keywords, url, search_type = "jobs", search_category = ""):
 		keyword = multiprocessing.Queue()
 		for key in keywords:
@@ -29,9 +27,10 @@ class WebScraper:
 
 		workers = []
 		for i in range(multiprocessing.cpu_count()):
-			worker = Worker(url, i, search_category, search_type, keyword, self.proxies, logger)
+			worker = Worker(url, i, search_category, search_type, keyword, logger)
 			workers.append(worker)
 
+		print "Scraping..."
 		for worker in workers:
 			worker.start()
 
@@ -43,16 +42,16 @@ class WebScraper:
 		logger.extracted_data.put(None) #poison pill to end the writer
 
 class Worker(multiprocessing.Process):
-	def __init__(self, url, id, search_category, search_type, keywords, proxies, logger):
+	def __init__(self, url, id, search_category, search_type, keywords, logger):
 		multiprocessing.Process.__init__(self)
 		self.ID = id
 		self.keyword = None
-		self.proxies = proxies
 		self.url = url
 		self.search_type = search_type
 		self.search_category = search_category
 		self.keyword_queue = keywords
 		self.logger = logger
+		self.job_entries = 0
 
 	def run(self):
 		while True:
@@ -62,51 +61,50 @@ class Worker(multiprocessing.Process):
 				break
 			else:
 				self.keyword = keyword
-				self.__scrape()
+				browser = Browser('phantomjs', service_args=proxies)
+				with browser:
+					browser.visit(self.url)
+					browser.fill('q', self.keyword)
+					browser.find_by_name('gosearch').click()
+					self.job_entries = 0
+					while True:
+						nxt = browser.find_by_css('li.next')
+						if len(nxt) > 0:
+							self.__scrape(browser.html.encode('utf-8'))
+							browser.find_by_css('li.next > a').first.click()
+						else:
+							self.__scrape(browser.html.encode('utf-8'))
+							break
+					print "Worker %d - %d entries found for the keyword '%s'" % (self.ID, self.job_entries, self.keyword)
 
-	def __scrape(self):
-		page = 1
+	def __scrape(self, text):
 		job_entries = 0
-		not_last_page = True
-		while not_last_page:
-			r = self.__send_request(page)
-			soup = BeautifulSoup(r.text)
-			no_result = soup.findAll(attrs = "no_entries")
-			if len(no_result) == 0:
-				content = soup.findAll('tr', attrs = {'class' : 'searchExcerpt'})
-				job = soup.findAll('tr', attrs = {'class' : 'aJobS'})
-				for i in range(len(job)):
-					job_title = job[i].find(attrs = 'jobTitleLink').text.encode("utf-8").strip()
-					try:
-						location = job[i].find(attrs = "address").text.strip() + ", "
-						location += job[i].find(attrs = "address").previous_element.previous_element.strip()
-					except:
-						location = job[i].findAll('td')[1].text.encode("utf-8").strip()
+		soup = BeautifulSoup(text)
+		content = soup.findAll('tr', attrs = {'class' : 'searchExcerpt'})
+		job = soup.findAll('tr', attrs = {'class' : 'aJobS'})
+		for i in range(len(job)):
+			job_title = job[i].find(attrs = 'jobTitleLink').text.encode("utf-8").strip()
+			try:
+				location = job[i].find(attrs = "address").text.strip() + ", "
+				location += job[i].find(attrs = "address").previous_element.previous_element.strip()
+			except:
+				location = job[i].findAll('td')[1].text.encode("utf-8").strip()
 
-					company = job[i].findAll('td')[2].text.encode("utf-8").strip()
-					try:
-						short_description = content[i].find(attrs = 'searchContent').text.encode("utf-8").strip()
-					except:
-						short_description = None
+			company = job[i].findAll('td')[2].text.encode("utf-8").strip()
+			try:
+				short_description = content[i].find(attrs = 'searchContent').text.encode("utf-8").strip()
+			except:
+				short_description = None
 
-					short_description = " ".join(short_description.split())
-					d = OrderedDict()
-					d['keyword'] = self.keyword
-					d['job_title'] = job_title
-					d['location'] = location
-					d['company'] = company
-					d['short_description'] = short_description
-					self.logger.extracted_data.put(d)
-					job_entries += 1
-				page += 1
-			else:
-				not_last_page = False
-		print "Worker %d - %d entries found for the keyword '%s'" % (self.ID, job_entries, self.keyword)
-
-	def __send_request(self, page):
-		self.url += str(self.search_type) + "/search/"
-		payload = {'q': self.keyword, 'searchType': self.search_type, 'searchCategory': self.search_category, 'page': page}
-		return requests.get(self.url, params = payload, proxies = self.proxies)
+			short_description = " ".join(short_description.split())
+			d = OrderedDict()
+			d['keyword'] = self.keyword
+			d['job_title'] = job_title
+			d['location'] = location
+			d['company'] = company
+			d['short_description'] = short_description
+			self.logger.extracted_data.put(d)
+			self.job_entries += 1
 
 class Logger(multiprocessing.Process):
 	def __init__(self, extracted_data):
@@ -126,6 +124,6 @@ class Logger(multiprocessing.Process):
 
 if __name__ == "__main__":
 	search = WebScraper()
-	keywords = ['python', 'php', 'developer', 'java', 'ruby', 'rail']
-	url = "http://mynimo.com/"
+	keywords = ['python', 'php', 'java', 'ruby', 'ada']
+	url = "http://cebu.mynimo.com/"
 	search.search(keywords, url)
